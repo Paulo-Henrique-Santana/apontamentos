b6dd21a29513bbbbc1f8163562b4c540dc0e0d65
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2025-08-19 18:43:01 -0300

    feat(createUser): validar e-mail já cadastrado antes da criação

--- backend/src/controllers/user/createUser.ts
+++ backend/src/controllers/user/createUser.ts
@@ -5,6 +5,15 @@ import { User } from "../../models/User";
 export const createUser = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Usuário já cadastrado com este e-mail" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
@@ -16,6 +25,8 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(201).json(user);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Ocorreu um erro inesperado ao criar usuário" });
+    return res
+      .status(500)
+      .json({ message: "Ocorreu um erro inesperado ao criar usuário" });
   }
-}
+};
